50b9bee279766f4a9db2edde2f55798beb0f02cb
Jackie <chowdhury1478@gmail.com>
2020-11-27 22:32:21 -0500

    first commit

---
+++ Expression.java
@@ -0,0 +1,17 @@
+public class Expression extends ExpressionTree {
+   
+   public String fullyParenthesized() {
+      // add implementation here
+      return "";
+   }
+
+   public Expression(String s) {
+      super();
+      // add implementation here
+   }
+   
+   public double evaluate() {
+      // add implementation here
+      return 0.0;
+   }
+}
