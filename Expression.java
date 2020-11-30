36cf33a7cae9233b7dfbed861bab7f517f4f48c4
Jackie <chowdhury1478@gmail.com>
2020-11-29 21:17:11 -0500

    added a root to the tree

--- Expression.java
+++ Expression.java
@@ -1,5 +1,6 @@
+
 public class Expression extends ExpressionTree {
-   
+   BNode<String> root ;
    public String fullyParenthesized() {
       // add implementation here
       return "";
@@ -8,10 +9,33 @@ public class Expression extends ExpressionTree {
    public Expression(String s) {
       super();
       // add implementation here
+      ExpressionTree tree = this;
+      try {
+
+
+         root = new BNode<String>("*",null,new BNode<String>("2",root,null,null) ,new BNode<String>("5",root,null,null));
+
+         //root.setRight();
+         //root.setLeft();
+
+         tree.addRoot(root.data);
+
+         treePrint(root);
+
+
+         System.out.println(tree.root);
+
+      }catch (Exception e){
+         System.out.println("");
+      }
+
+
+
    }
    
    public double evaluate() {
       // add implementation here
       return 0.0;
    }
+
 }
