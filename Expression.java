a36aa0505c3d066451a5774722170e2fa58aa2e2
Jackie <chowdhury1478@gmail.com>
2020-12-02 14:26:24 -0500

    parsing method now working

--- Expression.java
+++ Expression.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 
 public class Expression extends ExpressionTree {
 
@@ -9,13 +10,14 @@ public class Expression extends ExpressionTree {
    public Expression(String s) {
       super();
       // add implementation here
+      ArrayList<String> list = new ArrayList<>();
 
-      System.out.println(s);
-      System.out.println(isOperator(" +"));
-      System.out.println(isOperator("- "));
-      System.out.println(isOperator(" *"));
-      System.out.println(isOperator(" /"));
-      System.out.println(isDigit("25"));
+      parseInput(s,list);
+
+      for(int i =0; i < list.size(); i++){
+         System.out.println(list.get(i));
+      }
+
       try {
 
          BNode<String> node1 = new BNode<>("2",null,null,null);
@@ -46,20 +48,68 @@ public class Expression extends ExpressionTree {
       return 0.0;
    }
 
+   //----------------------------------------- Private Helper Methods -------------------------------//
 
-   private boolean isOperator(String op){
+   private boolean isOperator(char currChar){
       //remove the space from the operator
-      op = op.replace(" ", "");
-      return op.equals("+") || op.equals("-") || op.equals("*") || op.equals("/") ;
+
+      return currChar == '+' || currChar =='-' || currChar == '*' || currChar =='/' || currChar == '(' || currChar ==')';
    }
 
    private boolean isDigit(String num){
+
       try {
          double value = Double.parseDouble(num);
       }catch (NumberFormatException e){
          return  false;
       }
       return true;
+   }
+
+   //includes some recycled code from project 1 that breaks up input string into pieces.
+   private void parseInput(String s, ArrayList<String> list){
+      //get rid of spaces from the string
+      int i = 0;
+      String beforeCurrentChar;
+      String afterCurrentChar;
+
+      while (i < s.length()){
+         char currentChar = s.charAt(i);
+         if(currentChar == ' ' ){
+            beforeCurrentChar = s.substring(0,i); //everything that comes before currentChar
+            afterCurrentChar = s.substring(i+1); // everything that comes after currentChar
+            s = beforeCurrentChar + afterCurrentChar; // combine everything except the currentChar which essentially removes that char
+         }else{
+            i++;
+         }
+      }
+      System.out.println(s);
+
+
+      String sect = "";
+
+      //iterate through the string to break down the input by element
+      for(int k =0; k < s.length();k++){
+         char currChar = s.charAt(k);
+
+         //if we come across a + or a -, anything coming before it , up to the previous + or - is a term
+
+          if(!isOperator(currChar)) {
+            sect= ""+ currChar;
+         }else {
+             //if two operators are next to each other, it creates a white space. This avoids it.
+             if(sect.length() != 0) {
+                list.add(sect);
+             }
+             sect = "";
+             list.add(""+currChar);
+          }
+      }
+
+
+
+
+
    }
 
 
